6cb53706f8d1678a46c7617b1eb429b66d5773d6
Eric Smith <paytonrules@gmail.com>
2020-05-04 09:15:00 -0500

    Create a Globals struct in the lib
    This is just an attempt to import Globals. Note that Globals is
    uppercase here but lowercase in the GDScript code. May not matter
    because of how it is loaded.

--- src/air_combat/src/lib.rs
+++ src/air_combat/src/lib.rs
@@ -1,7 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use gdnative;
+use gdnative::{godot_gdnative_init, godot_gdnative_terminate, godot_nativescript_init};
+
+#[derive(gdnative::NativeClass)]
+#[inherit(gdnative::Node)]
+struct Globals;
+
+#[gdnative::methods]
+impl Globals {
+    fn _init(_owner: gdnative::Node) -> Self {
+        Globals
     }
 }
+
+fn init(handle: gdnative::init::InitHandle) {
+    handle.add_class::<Globals>();
+}
+
+godot_gdnative_init!();
+godot_nativescript_init!(init);
+godot_gdnative_terminate!();
